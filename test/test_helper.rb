4f6edc2a02a77401de68942be5c25c2128c1e2d4
Chris Hasiński <krzysztof.hasinski@gmail.com>
2026-01-08 22:49:40 +0100

    Sync test configuration with upstream Blazer
    - Add show_exceptions = :none to propagate errors in tests
    - Add load_defaults for Rails config
    - Remove sprockets from Combustion initialization
    - Add helper methods from upstream (stub_method, with_option, postgresql?)
    - Add test_schema to adapter tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,17 +2,20 @@ require "bundler/setup"
 require "combustion"
 Bundler.require(:default)
 require "minitest/autorun"
-require "minitest/pride"
 
 logger = ActiveSupport::Logger.new(ENV["VERBOSE"] ? STDERR : nil)
 
 Combustion.path = "test/internal"
-Combustion.initialize! :active_record, :action_controller, :action_mailer, :active_job, :sprockets do
+Combustion.initialize! :active_record, :action_controller, :action_mailer, :active_job do
+  config.load_defaults Rails::VERSION::STRING.to_f
   config.action_controller.logger = logger
   config.action_mailer.logger = logger
   config.active_job.logger = logger
   config.active_record.logger = logger
   config.cache_store = :memory_store
+
+  # fixes warning with adapter tests
+  config.action_dispatch.show_exceptions = :none
 end
 
 Rails.cache.logger = logger
@@ -29,6 +32,32 @@ class ActionDispatch::IntegrationTest
 
   def create_check(**attributes)
     Blazer::Check.create!(schedule: "5 minutes", **attributes)
+  end
+
+  def postgresql?
+    ENV["ADAPTER"].nil?
+  end
+
+  def with_option(name, value)
+    previous_value = Blazer.send(name)
+    begin
+      Blazer.send("#{name}=", value)
+      yield
+    ensure
+      Blazer.send("#{name}=", previous_value)
+    end
+  end
+
+  def stub_method(cls, method, code)
+    original_code = cls.method(method)
+    begin
+      cls.singleton_class.undef_method(method)
+      cls.define_singleton_method(method, code.respond_to?(:call) ? code : ->(*) { code })
+      yield
+    ensure
+      cls.singleton_class.undef_method(method) if cls.singleton_class.method_defined?(method)
+      cls.define_singleton_method(method, original_code)
+    end
   end
 end
 
